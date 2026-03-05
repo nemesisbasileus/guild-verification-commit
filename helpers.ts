a6942e92601bcd7fe592f125596ca2c67e35855f
nemesisbasileus <91253198+nemesisbasileus@users.noreply.github.com>
2026-03-06 00:50:12 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T21:50:11.648Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-05T21:50:12.870Z
+Commit: Fix broken links in documentation
