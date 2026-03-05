544cfb102939ce3e539d6213bc3baba02015681f
nemesisbasileus <91253198+nemesisbasileus@users.noreply.github.com>
2026-03-06 00:50:24 +0300

    Patch security vulnerability in authentication

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T21:49:53.377Z
-Commit: Add multi-language support
+Timestamp: 2026-03-05T21:50:25.230Z
+Commit: Patch security vulnerability in authentication
