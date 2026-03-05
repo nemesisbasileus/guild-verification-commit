770a76e42aa01e311d64071dcb1b2799869a2e00
nemesisbasileus <91253198+nemesisbasileus@users.noreply.github.com>
2026-03-06 00:49:52 +0300

    Add multi-language support

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T21:49:40.745Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-05T21:49:53.377Z
+Commit: Add multi-language support
