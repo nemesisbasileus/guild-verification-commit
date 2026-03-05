374ff47318619469c0490ae4debb849a24786e3e
nemesisbasileus <91253198+nemesisbasileus@users.noreply.github.com>
2026-03-06 00:51:06 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T21:50:31.614Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-05T21:51:06.784Z
+Commit: Fix broken links in documentation
