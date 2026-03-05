e1530f8f194f2a721692d1cef22162a42e3a7141
nemesisbasileus <91253198+nemesisbasileus@users.noreply.github.com>
2026-03-06 00:50:17 +0300

    Add multi-language support

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T21:50:03.107Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-05T21:50:18.283Z
+Commit: Add multi-language support
