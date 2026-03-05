80f3be6dfc36a4e33b49e16bf52c3424838d9822
nemesisbasileus <91253198+nemesisbasileus@users.noreply.github.com>
2026-03-06 00:49:36 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T21:49:35.572Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-05T21:49:36.840Z
+Commit: Patch API endpoint error handling
