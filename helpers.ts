93fd7e6e80d54f0a6b877b5a5165411603373097
nemesisbasileus <91253198+nemesisbasileus@users.noreply.github.com>
2026-03-06 00:51:26 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T21:51:13.981Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-05T21:51:27.079Z
+Commit: Fix broken links in documentation
